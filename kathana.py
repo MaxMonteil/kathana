7b8c310ea007db7bb941649780582d345cc10bdc
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-03-31 01:08:17 +0300

    Run black formatter.

--- kathana.py
+++ kathana.py
@@ -1,17 +1,22 @@
 import os
 from resources.asanacomm import Asanacomm
 
-WORKSPACE = 'Accessibility Web Engine'
-OUT_DIR = './reports'
+WORKSPACE = "Accessibility Web Engine"
+OUT_DIR = "./reports"
 
 
 def main():
-    acomm = Asanacomm(os.environ['ASANA_TOKEN'], WORKSPACE, OUT_DIR,
-                      start_date='2019-03-18', verbose=True)
+    acomm = Asanacomm(
+        os.environ["ASANA_TOKEN"],
+        WORKSPACE,
+        OUT_DIR,
+        start_date="2019-03-18",
+        verbose=True,
+    )
 
     acomm.generate_report()
     acomm.write_report_to_file()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
