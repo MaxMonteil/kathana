179d91e2cdf3d1617a219822a9a58374fa68016e
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-05-05 14:05:22 +0300

    Implement logging based on verbosity

--- services/write_to_file.py
+++ services/write_to_file.py
@@ -4,6 +4,8 @@ from pathlib import Path
 def write(
     self, *, report, report_format, out_dir, start_date, verbose=True
 ):
+    log = print if verbose else lambda *a, **k: None
+
     out_dir = Path(out_dir)
     if not out_dir.suffix:
         file_name = start_date.replace(" ", "") + "-report." + report_format
@@ -12,10 +14,10 @@ def write(
     if not Path.exists(file_path):
         Path.touch(file_path, exist_ok=True)
 
-    self._log("\nWriting report to file...")
+    log("\nWriting report to file...")
     with open(file_path, "w") as out:
         out.write(report)
 
-    self._log(f"Done!")
-    self._log("\nReport written here:", end=" ")
-    self._log(Path.joinpath(Path.cwd(), file_path))
+    log(f"Done!")
+    log("\nReport written here:", end=" ")
+    log(Path.joinpath(Path.cwd(), file_path))
